c8c4dfdeb98b40ccadabd181829ab7e8c17d1b93
srinivas <gantasrinu019@gmail.com>
2023-12-08 13:17:44 +0530

    code

--- sample.sh
+++ sample.sh
@@ -1,2 +1,8 @@
 echo -e "\e[31m Installing nginx service \e[0m"
 yum install nginx -y
+echo -e "\e[32m removing default content \e[0m"
+cd /usr/share/nginx/html
+rm -rf *
+echo -e "\e[33m Downloading new app content\e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+unzip frontend.zip
