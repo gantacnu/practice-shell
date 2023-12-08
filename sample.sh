261cd880d19cff0185846860d5da9f8df4a17309
srinivas <gantasrinu019@gmail.com>
2023-12-08 13:21:52 +0530

    code

--- sample.sh
+++ sample.sh
@@ -6,3 +6,6 @@ rm -rf *
 echo -e "\e[33m Downloading new app content\e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
+echo -e "\e[34m Enabling and starting nginx service\e[0m"
+systemctl enable nginx
+systemctl restart nginx
