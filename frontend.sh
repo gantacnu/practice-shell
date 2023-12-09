cdacb28b0bef4bec760045cf09ebed3dd5d13f8b
srinivas <gantasrinu019@gmail.com>
2023-12-09 11:30:09 +0530

    code

--- frontend.sh
+++ frontend.sh
@@ -1,2 +1,10 @@
 echo -e "\e[31m Installing Nginx Service \e[0m"
 yum install nginx -y
+echo -e "\e[31m Removing default server content\e[0m"
+cd /usr/share/nginx/html
+rm -rf *
+echo -e"\e[31m Downloading custom server content \e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+unzip frontend.zip
+systemctl enable nginx
+systemctl start nginx
